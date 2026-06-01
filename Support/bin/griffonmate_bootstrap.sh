dfd211c565112c4a7ad371cd5a37fe1ae2653450
Shane Becker <veganstraightedge@gmail.com>
2026-05-31 23:42:30 -0700

    shebangs: ruby18/ruby20 -> ruby; drop 1.8-only flags (-K*, -rjcode)

--- Support/bin/griffonmate_bootstrap.sh
+++ Support/bin/griffonmate_bootstrap.sh
@@ -5,4 +5,4 @@ export TM_GRIFFON=`which $TM_GRIFFON`
 script="$1"
 shift
 
-ruby18 -r GriffonMate -r ui -- "$TM_BUNDLE_SUPPORT/GriffonMate/$script.rb" $@
+ruby -r GriffonMate -r ui -- "$TM_BUNDLE_SUPPORT/GriffonMate/$script.rb" $@
